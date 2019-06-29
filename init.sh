a53b20cea0bfebe7075c781ee54532c609c857ea
482F <482F.git.mail@482f.net>
2019-06-30 00:00:00 +0900

    カレントディレクトリ関連の修正

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ set -ue -o pipefail
 
 SCRIPT_DIR=$(cd $(dirname ${0}); pwd)
 
+cd "${SCRIPT_DIR}"
+
 git clone https://github.com/vbaidiot/Ariawase
 
 mv "${SCRIPT_DIR}/Ariawase/vbac.wsf" ./vbac.wsf
@@ -12,5 +14,9 @@ rm "${SCRIPT_DIR}/Ariawase" -rf
 mkdir "${SCRIPT_DIR}/bin"
 mkdir "${SCRIPT_DIR}/src"
 
+cd ..
+mv ./easy_excel_git/* ./
+rm ./easy_excel_git -rf
+rm ./init.sh
 
 exit 0
