911adc263bb1e187c4b09084c03dcb6a108c1b93
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-07-02 16:00:30 +0900

    tr で同じファイルにリダイレクトすると内容が消えたので修正

--- convert_lf.sh
+++ convert_lf.sh
@@ -3,8 +3,9 @@
 set -ue -o pipefail
 
 find ./src/ -type f | while read file; do
-    tr -d '\r' < ${file} > ${file}
+    tr -d '\r' < "${file}" > "${file}_lf"
+    rm "${file}"
+    mv "${file}_lf" "${file}"
 done
 
 exit 0
-
