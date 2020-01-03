40e28a20d3f33a1d067b47ba4e82fba40df387dd
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-01-03 20:01:35 +0900

    import.bat で確認をするように

--- import.bat
+++ import.bat
@@ -1,1 +1,14 @@
-cscript .\easy_excel_git\vbac.wsf combine
+@echo off
+
+set R=%RANDOM%
+set R=%RANDOM%
+echo %R%
+set /P USER_INPUT="input above number to do import: "
+
+if not %R%==%USER_INPUT% (
+    echo your input doesn't correct. stop processing.
+) else (
+    cscript .\easy_excel_git\vbac.wsf combine
+)
+
+pause
