1913ea3fc75d5fc9004fc92d5a9fedb2df045901
482F <482F.git.mail@482f.net>
2020-01-05 00:00:00 +0900

    誤import 防止用にリポジトリ名の末尾 10 文字を入力させるように

--- import.bat
+++ import.bat
@@ -1,5 +1,16 @@
 @echo off
 
+set NAME=%CD%
+set NAME=%NAME:~-10%
+echo %NAME%
+set /P USER_INPUT="input above string to do import: "
+
+if not %NAME%==%USER_INPUT% (
+    echo your input doesn't correct. stop processing.
+    pause
+    exit
+)
+
 set R=%RANDOM%
 set R=%RANDOM%
 echo %R%
